540711d9dafe0198cab3d4bc2ff09c019c533d94
YifanYang6 <yifanyang6@link.cuhk.edu.cn>
2025-12-09 20:46:00 +0800

    enable http
    Signed-off-by: YifanYang6 <yifanyang6@link.cuhk.edu.cn>

--- otel.js
+++ otel.js
@@ -64,9 +64,6 @@
             '@opentelemetry/instrumentation-net': {
               enabled: false, // Disable net instrumentation
             },
-            '@opentelemetry/instrumentation-http': {
-              enabled: false, // Disable http instrumentation
-            },
           }),
         ],
         logRecordProcessor: new BatchLogRecordProcessor(logExporter),
